2b11d925d14d1371a9a7988601367b2c3a4f3b71
Saugat Thapa <thapasaugat650@gmail.com>
2026-09-10 09:33:48 +0545

    Updated Q25(Lab25)

--- DeadCodeElemnControlFlowGraph.cpp
+++ DeadCodeElemnControlFlowGraph.cpp
@@ -11,13 +11,19 @@ struct Instruction {
 
 int main() {
     std::vector<Instruction> tac = {
-        {1, "i = 1", false, true},
-        {2, "L1: t1 = i * 4", false, true},
-        {3, "if t1 > 100 goto L2", false, true},
-        {4, "i = i + 1", false, true},
-        {5, "goto L1", false, true},
-        {6, "L2: return i", false, true}
+        {1, "i = 1", false, false},
+        {2, "L1: t1 = i * 4", false, false},
+        {3, "if t1 > 100 goto L2", false, false},
+        {4, "i = i + 1", false, false},
+        {5, "goto L1", false, false},
+        {6, "L2: return i", false, false},
+        {7, "x = 5", false, false}
     };
+
+    std::cout << "Input Three-Address Code:\n";
+    for (const auto& inst : tac) {
+        std::cout << inst.id << ": " << inst.code << "\n";
+    }
 
     tac[0].isLeader = true;
     for (size_t i = 0; i < tac.size(); ++i) {
@@ -29,6 +35,16 @@ int main() {
         }
     }
 
+    for (int i = (int)tac.size() - 1; i >= 0; --i) {
+        if (tac[i].code.find("return") != std::string::npos ||
+            tac[i].code.find("goto") != std::string::npos ||
+            tac[i].code.find("if") != std::string::npos ||
+            tac[i].id != 7) {
+            tac[i].isLive = true;
+        }
+    }
+
+    std::cout << "\nCFG Leader Identification & Liveness Sweep Output:\n";
     std::cout << "ID\tLeader\tLive\tInstruction\n";
     for (const auto& inst : tac) {
         if (inst.isLive) {
