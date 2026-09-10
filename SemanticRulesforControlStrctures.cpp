8e5d37fd3affed7117ef90ec9e75cdb08e7d4391
Saugat Thapa <thapasaugat650@gmail.com>
2026-09-10 09:05:34 +0545

    Updated Q19(Lab19)

--- SemanticRulesforControlStrctures.cpp
+++ SemanticRulesforControlStrctures.cpp
@@ -1,15 +1,8 @@
 #include <iostream>
-#include <vector>
 #include <string>
 #include <unordered_map>
 
 enum DataType { TYPE_INT, TYPE_FLOAT, TYPE_BOOL, TYPE_ERROR };
-
-struct SymbolState {
-    bool is_initialized;
-};
-
-std::unordered_map<std::string, SymbolState> symbolTable;
 
 class IfASTNode;
 class WhileASTNode;
@@ -17,15 +10,18 @@ class ForASTNode;
 class BreakASTNode;
 class ContinueASTNode;
 class VarASTNode;
+class ExprASTNode;
 
 class SemanticVisitor {
 public:
+    virtual ~SemanticVisitor() = default;
     virtual DataType visitIfNode(IfASTNode* node) = 0;
     virtual DataType visitWhileNode(WhileASTNode* node) = 0;
     virtual DataType visitForNode(ForASTNode* node) = 0;
     virtual DataType visitBreakNode(BreakASTNode* node) = 0;
     virtual DataType visitContinueNode(ContinueASTNode* node) = 0;
     virtual DataType visitVarNode(VarASTNode* node) = 0;
+    virtual DataType visitExprNode(ExprASTNode* node) = 0;
 };
 
 class ASTNode {
@@ -35,45 +31,21 @@ public:
     virtual DataType accept(SemanticVisitor* visitor) = 0;
 };
 
+class ExprASTNode : public ASTNode {
+public:
+    DataType type;
+    ExprASTNode(DataType t, int line) : type(t) { lineNo = line; }
+    DataType accept(SemanticVisitor* visitor) override {
+        return visitor->visitExprNode(this);
+    }
+};
+
 class VarASTNode : public ASTNode {
 public:
     std::string name;
     VarASTNode(std::string n, int line) : name(n) { lineNo = line; }
     DataType accept(SemanticVisitor* visitor) override {
         return visitor->visitVarNode(this);
-    }
-};
-
-class WhileASTNode : public ASTNode {
-public:
-    ASTNode* condition;
-    ASTNode* body;
-    WhileASTNode(ASTNode* cond, ASTNode* b, int line)
-        : condition(cond), body(b) { lineNo = line; }
-    DataType accept(SemanticVisitor* visitor) override {
-        return visitor->visitWhileNode(this);
-    }
-};
-
-class ForASTNode : public ASTNode {
-public:
-    ASTNode* condition;
-    ASTNode* body;
-    ForASTNode(ASTNode* cond, ASTNode* b, int line)
-        : condition(cond), body(b) { lineNo = line; }
-    DataType accept(SemanticVisitor* visitor) override {
-        return visitor->visitForNode(this);
-    }
-};
-
-class IfASTNode : public ASTNode {
-public:
-    ASTNode* condition;
-    ASTNode* body;
-    IfASTNode(ASTNode* cond, ASTNode* b, int line)
-        : condition(cond), body(b) { lineNo = line; }
-    DataType accept(SemanticVisitor* visitor) override {
-        return visitor->visitIfNode(this);
     }
 };
 
@@ -93,13 +65,49 @@ public:
     }
 };
 
+class WhileASTNode : public ASTNode {
+public:
+    ASTNode* condition;
+    ASTNode* body;
+    WhileASTNode(ASTNode* cond, ASTNode* b, int line) : condition(cond), body(b) { lineNo = line; }
+    DataType accept(SemanticVisitor* visitor) override {
+        return visitor->visitWhileNode(this);
+    }
+};
+
+class ForASTNode : public ASTNode {
+public:
+    ASTNode* condition;
+    ASTNode* body;
+    ForASTNode(ASTNode* cond, ASTNode* b, int line) : condition(cond), body(b) { lineNo = line; }
+    DataType accept(SemanticVisitor* visitor) override {
+        return visitor->visitForNode(this);
+    }
+};
+
+class IfASTNode : public ASTNode {
+public:
+    ASTNode* condition;
+    ASTNode* body;
+    IfASTNode(ASTNode* cond, ASTNode* b, int line) : condition(cond), body(b) { lineNo = line; }
+    DataType accept(SemanticVisitor* visitor) override {
+        return visitor->visitIfNode(this);
+    }
+};
+
+std::unordered_map<std::string, bool> symbolTable;
+
 class ConcreteSemanticVisitor : public SemanticVisitor {
 private:
     int loop_depth = 0;
 
 public:
+    DataType visitExprNode(ExprASTNode* node) override {
+        return node->type;
+    }
+
     DataType visitVarNode(VarASTNode* node) override {
-        if (!symbolTable[node->name].is_initialized) {
+        if (!symbolTable[node->name]) {
             std::cout << "Line " << node->lineNo << ": Error Variable '" << node->name << "' used before initialization.\n";
             return TYPE_ERROR;
         }
@@ -155,15 +163,21 @@ public:
 };
 
 int main() {
-    symbolTable["a"] = {false};
     ConcreteSemanticVisitor visitor;
+    symbolTable["a"] = false;
 
-    VarASTNode uninitVar("a", 1);
+    std::cout << "Input Statement 1: Evaluated 'break' statement at Line 2 outside loop\n";
     BreakASTNode badBreak(2);
     badBreak.accept(&visitor);
 
-    WhileASTNode whileNode(&uninitVar, nullptr, 3);
-    whileNode.accept(&visitor);
+    std::cout << "\nInput Statement 2: Evaluated uninitialized variable 'a' at Line 1\n";
+    VarASTNode uninitVar("a", 1);
+    uninitVar.accept(&visitor);
+
+    std::cout << "\nInput Statement 3: Evaluated while loop at Line 3 with float condition\n";
+    ExprASTNode floatCond(TYPE_FLOAT, 3);
+    WhileASTNode badWhile(&floatCond, nullptr, 3);
+    badWhile.accept(&visitor);
 
     std::cout << "\nLab No. 19 Name: Saugat Bikram Thapa /Roll No.: 80117731/ Section: A\n";
     return 0;
